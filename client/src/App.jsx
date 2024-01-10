5464b5cf27afa7f9abdc87f962f9c4bced5b86cd
Hanzalashaik <shaikhhanzala27@gmail.com>
2024-01-10 11:15:57 +0000

    Completed✅

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+function App() {
+  return (
+    <>
+      <div className="bg-gradient-to-r from-green-400 to-blue-500 w-screen h-screen">
+        <div className="p-4">
+          <h1 className="text-3xl font-bold">Hanzala Shaikh🚀</h1>
+          <h2 className="mb-4 font-semibold">Full Stack Web Developer👨‍💻</h2>
+          <div>
+            <h1 className="text-2xl font-semibold">✅Projects</h1>
+            <ul className="list-disc ml-5 ">
+              <li className="hover:underline text-blue-700 font-medium"><a href="">Project Management Mern App</a></li>
+              <li className="hover:underline text-blue-700 font-medium"><a href="https://todo.hanzala.site/">To-do Mern App</a></li>
+              <li className="hover:underline text-blue-700 font-medium"><a href="https://react.hanzala.site/">React Essential App</a></li>
+              <li className="hover:underline text-blue-700 font-medium"><a href="https://tictactoe.hanzala.site/">Tic Tac Toe Game</a></li>
+              <li className="hover:underline text-blue-700 font-medium"><a href="https://investment.hanzala.site/">Investment App</a></li>
+              <li className="hover:underline text-blue-700 font-medium"><a href="https://countdown.hanzala.site/">coutDown Game</a></li>
+              <li className="hover:underline text-blue-700 font-medium"><a href="https://quizapp.hanzala.site/">Quiz App</a></li>
+              <li className="hover:underline text-blue-700 font-medium"><a href="https://videohub-hanzalashaikh.vercel.app/">Vido Hub</a></li>
+              <li className="hover:underline text-blue-700 font-medium"><a href="https://reactoneproject-umber.vercel.app/">Zone Demo App</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
